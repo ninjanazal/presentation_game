b1b3e57df7cca70755f3095801c17bdb3562a6b9
Eurico Martins <euricomartinss@gmail.com>
2021-01-31 15:00:54 +0000

    add progress bar

--- presentationApp/src/scripts/finding_behavior.gd
+++ presentationApp/src/scripts/finding_behavior.gd
@@ -1,14 +1,15 @@
 extends Node
 
 export (Array, Resource) var states
-
 export(float) var loadingAngularSpeed = 25.0
+
 onready var _loading_texture := get_node("TextureRect")
 onready var _info_lable := get_node("info_lable")
-
+onready var _progress_bar := get_node("ProgressBar")
 
 func _ready():
 	_info_lable.set_text("- - - - -")
+	_progress_bar.set_value(0)
 
 func _process(delta):
 	_loading_texture.set_rotation_degrees(_loading_texture.get_rotation_degrees() +\
@@ -25,6 +26,7 @@ func _displayInformationCycle():
 		_info_lable.set_text(states[currentMessage].getText());
 		yield(get_tree().create_timer(states[currentMessage].getTimeOn()), "timeout")
 		currentMessage +=1
+		_progress_bar.set_value((currentMessage * 100) / (states.size() -1))
 	
 	MainController.SwitchTo(2)
 	_info_lable.set_text("- - - - -")
